62e2ee921823c7bb7f89b5e9d90156dd3fdc11f6
Kumegawwa <2742097@fmu.edu.br>
2024-09-23 16:22:11 -0300

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,6 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext('2d');
 
-const audio = document.getElementById("myAudio");
-        audio.volume = 0.09;
 let mouseMoved = false;
 
 const pointer = {
